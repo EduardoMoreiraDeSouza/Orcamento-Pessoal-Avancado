fde9179bc2571b51061c97cd975d5feba55f573d
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2024-06-30 22:37:17 -0300

    Update EntradaDadosBancosCorretoras.php

--- backEnd/dados/EntradaDadosBancosCorretoras.php
+++ backEnd/dados/EntradaDadosBancosCorretoras.php
@@ -6,7 +6,7 @@ abstract class EntradaDadosBancosCorretoras extends ObterDadosBancosCorretoras
 {
 	protected function EntradaDadosBancosCorretoras($bancoCorretora)
 	{
-		$this -> setCodigoMySql("INSERT INTO dbName.bancosCorretoras VALUES ('0', '$bancoCorretora', '" . $this -> getSessao() . "', '');");
+		$this -> setCodigoMySql("INSERT INTO dbName.bancosCorretoras VALUES ('0', '$bancoCorretora', '" . $this -> getSessao() . "', '0');");
 
 		return (bool) $this -> ExecutarCodigoMySql();
 	}
