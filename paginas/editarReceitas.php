12e685f6cf874947a147a51d7d63310fd0e3a7f4
Eduardo Moreira (Moreira) <edmoreira043557@gmail.com>
2024-07-01 21:29:06 -0300

    Correções semânticas

--- paginas/editarReceitas.php
+++ paginas/editarReceitas.php
@@ -31,57 +31,8 @@ if ($login -> VerificarLogin()) {
 	</head>
 
 	<body>
-	<nav class="navbar navbar-dark bg-dark fixed-top">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="../index.php">< Orçamento Pessoal ></a>
 
-			<button class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
-			        data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar"
-			        aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
-
-			<div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar"
-			     aria-labelledby="offcanvasDarkNavbarLabel">
-
-				<div class="offcanvas-header">
-					<h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">< Receitas ></h5>
-					<button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas"
-					        aria-label="Close"></button>
-				</div>
-
-				<div class="offcanvas-body">
-					<ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="../paginas/bancosCorretoras.php">Bancos/Corretoras</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="../paginas/gastos.php">Gastos</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="../paginas/cartaoCredito.php">Cartões de Crédito</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="#">Reserva de Emergência (Em Breve)</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="#">Investimentos (Em Breve)</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link text-light" href="#">Rendimentos (Em Breve)</a>
-						</li>
-						<li class="nav-item h6">
-							<a class="nav-link active text-light" aria-current="page"
-							   href="../backEnd/InteracaoFront/sair.php">Sair</a>
-						</li>
-
-					</ul>
-
-				</div>
-			</div>
-		</div>
-	</nav>
+	<?php require(__DIR__ . "/./particoes/menu/menu.php"); ?>
 
 	<div class="box">
 		<section class="banner" id="banner">
